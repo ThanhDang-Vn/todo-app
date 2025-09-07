fb5208106377e3ab14d9e599b00531f8cd8f7fcf
ThanhDang-Vn <ntdang23@clc.fitus.edu.vn>
2025-09-07 10:27:37 +0700

    custom mode toggle

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,20 +1,29 @@
-import { HomeIcon, UserRound, FileBox } from 'lucide-react';
+import { LayoutDashboard, CircleUserRound, FileBox, Github } from 'lucide-react';
 import { NavButton } from '@/app/components/NavButton';
 import Link from 'next/link';
+import { ModeToggle } from '@/app/components/ModeToggle';
 
 export function Header() {
   return (
-    <header className='bg-background h-12 p-2 border-b sticky top-0 z-20 flex items-center justify-between w-full bg-red-500'>
-      <div className='flex items-center gap-2'>
-        <NavButton href='/home' label='Home' icon={HomeIcon} variant='ghost' />
+    <header className='bg-background h-12 p-2 border-b sticky top-0 z-20'>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-2'>
+          <NavButton href='/home' label='Home' icon={LayoutDashboard} variant='ghost' />
 
-        <Link href={'/home'} className='flex justify-center items-center gap-2 ml-0' title='home'>
-          <h1 className='text-sm font-semibold '>Basic link to home</h1>
-        </Link>
-      </div>
+          <Link href={'/home'} className='flex justify-center items-center gap-2 ml-0' title='home'>
+            <h1 className='text-sm font-semibold'>Home</h1>
+          </Link>
 
-      <div className='flex items-center'>
-        <h1> Right </h1>
+          <Link href={'/customer'} className='flex justify-center items-center gap-2 ml-0' title='customer'>
+            <h1 className='test-sm font-semibold'>Customer</h1>
+          </Link>
+        </div>
+
+        <div className='flex items-center'>
+          <NavButton href='https://github.com/ThanhDang-Vn/todo-app' label='Github' icon={Github} variant='ghost' />
+          <ModeToggle />
+          <NavButton href='/profile' label='Profile' icon={CircleUserRound} variant='ghost' />
+        </div>
       </div>
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import { Header } from './components/Header';
+import { ModeToggle } from './components/ModeToggle';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/app/components/ui/dropdown-menu';
 
 export default function home() {
   return (
     <div className='flex justify-between items-center'>
-      <div className='flex items-center'>Hello</div>
-      <div className='flex items-center'>Hello</div>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
